8b235edca7e4197a8a1a99452c09c8d1071f48ed
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-04-19 16:52:59 +0100

    Introduced hash widget file in workflow

--- scripts/build-widget.sh
+++ scripts/build-widget.sh
@@ -21,7 +21,7 @@ if [ "$COUNT" -ne 1 ]; then
 fi
 
 # IMPORTANT: keep base64 (NOT url-safe) for SRI
-HASH=$(openssl dgst -sha256 -binary "$FILE" | openssl base64 -A)
+HASH=$(openssl dgst -sha256 -binary "$FILE" | openssl base64 -A | tr '+/' '-_' | tr -d '=')
 
 NEW_FILE="${BASE_DIR}/widget-${WIDGET_NAME}@${HASH}.iife.js"
 
