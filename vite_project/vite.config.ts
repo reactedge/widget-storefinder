fb0e70b0c2396f878820ac0bfb7ba4509777235b
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-04-19 19:57:28 +0100

    Unified deployment flow with other widgets to use hash files

--- vite_project/vite.config.ts
+++ vite_project/vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 import pkg from './package.json'
 import { manifestPlugin } from './manifestPlugin'
 
+const widgetName = 'storefinder';
 export default defineConfig({
   plugins: [
     react(),
-    manifestPlugin({ widgetName: 'storefinder' })
+    manifestPlugin({ widgetName }),
   ],
   define: {
-    'process.env': {},
+    'process.env.WIDGET_NAME': JSON.stringify(widgetName)
   },
   build: {
     outDir: "../www",
@@ -17,17 +18,17 @@ export default defineConfig({
     emptyOutDir: false,
     lib: {
       entry: "src/widget.ts",
-      name: "WidgetStoreFinder",
-      fileName: () => `widget-storefinder@${pkg.version}.iife.js`,
+      name: "WidgetUsp",
+      fileName: () => `widget-${widgetName}@${pkg.version}.iife.js`,
       formats: ["iife"],
     },
     rollupOptions: {
       output: {
         inlineDynamicImports: true,
-        assetFileNames: "widget-storefinder.[ext]",
+        assetFileNames: `widget-${widgetName}.[ext]`,
       },
     },
     minify: true,
     sourcemap: false
   }
-});
+})
